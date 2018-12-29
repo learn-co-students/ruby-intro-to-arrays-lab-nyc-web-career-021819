f177197ade2743b710e99dfe72529442f55270c7
Khaled M. Khalifeh <github email address>
2018-12-29 17:01:40 +0200

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,40 @@
+def instantiate_new_array
+	my_new_array = []
+	my_new_array
+end
+
+def array_with_two_elements
+	my_new_array = ["element1", "element2"]
+	my_new_array
+end
+
+def first_element(array)
+	element1 = array[0]
+	element1
+end
+
+def third_element(array)
+	element3 = array[2]
+	element3
+end
+
+def last_element(array)
+	last_el = array[-1]
+	last_el
+end
+
+def first_element_with_array_methods(array)
+	element1 = array.first
+	element1
+end
+
+def last_element_with_array_methods(array)
+	last_el = array.last
+	last_el
+end
+
+def length_of_array(array)
+	length = array.length
+	length
+end
+
