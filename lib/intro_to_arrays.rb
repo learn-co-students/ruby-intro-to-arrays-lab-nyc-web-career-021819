1e30b1aefa14c8b73f86091a4ffa5c5622b680f0
Ashley Archibald <aarchibald20@gmail.com>
2019-02-12 12:22:36 +0000

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -3,37 +3,37 @@ def instantiate_new_array
   @my_new_array = instantiate_new_array
 end
 
-def array_with_two_elements
-  array_with_two_elements = [3,4]
+ def array_with_two_elements
+  array_with_two_elements = [1, 2]
   @my_two_array = array_with_two_elements
 end
 
-def first_element(my_first_element)
-  @taylor_swift = ["Hello", "GoodBye", "Yes"]
-   my_first_element = first_element(@taylor_swift)
+ def first_element(my_first_element)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_first_element = @taylor_swift.first
 end
 
-def third_element(my_third_element)
-  @taylor_swift = ["Hello", "GoodBye", "Style"]
-  my_last_element = @taylor_swift[-1]
+ def third_element(my_third_element)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_third_element = @taylor_swift[2]
 end
 
-def last_element(my_last_element)
+ def last_element(my_last_element)
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   my_last_element = @taylor_swift[-1]
 end
 
-
-def first_element_with_array_methods(first_country)
+ def first_element_with_array_methods(first_country)
   @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
   first_country = @south_east_asia.first
 end
 
-def last_element_with_array_methods(last_country)
+ def last_element_with_array_methods(last_country)
   @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
- last_country = @south_east_asia.last
+  last_country = @south_east_asia.last
 end
 
-def length_of_array(length)
-  @programming_languages = ["C++", "Java", "Python", "Java"]
+ def length_of_array(length)
+  @programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+  length = @programming_languages.length
 end
